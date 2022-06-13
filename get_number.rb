e2285a6f409f3119afce5490df462e2fb333e1fc
RobinSheps <robin.shepheard@protonmail.com>
2022-06-13 14:56:15 +0000

    Add guess tracking

--- get_number.rb
+++ get_number.rb
@@ -9,3 +9,13 @@ input = gets
 name =input.chomp
 
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess
+puts "I've got a random number between 1 and 100."
+puts "Can you guess what it is?"
+target = rand(100)+1
+
+# Track the number of guesses the player has made.
+num_guesses = 0
+
+puts "You've got #{10 - num_guesses} guesses left."
